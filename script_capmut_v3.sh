07f485a6e979952e4114b1e7e016cc7fd0e88411
tprunarsson <tpr@hi.is>
2026-02-04 15:26:55 +0000

    ..

--- script_capmut_v3.sh
+++ script_capmut_v3.sh
@@ -6,7 +6,6 @@ BIN_DIR="${ROOT_DIR}/bin"
 SOL_DIR="${ROOT_DIR}/sol"
 DAT_FILE="${ROOT_DIR}/dat/singleboatdata2023spring.dat"
 
-TIME_LIMIT=300
 CAP_TIMELIMITS=(60 120 180 240 300 360 420 480)
 
 mkdir -p "${SOL_DIR}"
@@ -16,7 +15,6 @@ for cap_t in "${CAP_TIMELIMITS[@]}"; do
   out_log="${ROOT_DIR}/capmut_v3_${cap_t}.txt"
   echo "Running cap-time-limit=${cap_t} -> ${out_dat}"
   "${BIN_DIR}/matcapmutheur_v3" "${DAT_FILE}" 1 \
-    --time-limit "${TIME_LIMIT}" \
     --cap-time-limit "${cap_t}" \
     --write-dat "${out_dat}" \
     --verbose-init \
